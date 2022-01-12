53d92b426931f0a49ee1a7f15577b9debbfe526a
Tony Linz <tonyllinz@gmail.com>
2022-01-11 18:41:42 -0600

    add modal and form basics to html, create getParkInfo function

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,26 @@
+var searchFormEl = document.getElementById("state-search-form");
+
 //get the modal form to accept a state and populate the container 2 info
+var getParkInfo = function (event) {
+
+    var stateInputEl = document.querySelector("#state");
+    var stateAbbr = stateInputEl.value.trim();  
+    
+    //need to add query parameter and hopefully be able to use state without having to convert state to anything queryable
+    var apiUrl = "uhttps://developer.nps.gov/api/v1/parks?" + state + "&api_key=LlVYiDWyyOiv7SJeWDVIbQAJ2mMuYi64fapw7tEA";
+
+    fetch(apiUrl)
+        .then(function(response) {
+            if (response.ok) {
+                response.json().then(generateCards(data));
+            }
+        })
+        .catch(function(error) {
+            //notice this '.catch()' getting chained onto the end of the '.then()' method
+            console.log(error);
+        });
+}
+
 
     //prevent default reload for form submission
 
@@ -38,6 +60,7 @@
 
 
 //add event listener for form submission
+searchFormEl.addEventListener("submit", getParkInfo);
 
 //add event listener for when a card is clicked in container two
 
